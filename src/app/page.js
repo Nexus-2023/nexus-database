26f39ddbeb2c6ae28529753d3c1559a66c535af6
Muhammadumer.eth <umershaikh217@gmail.com>
2024-01-04 23:31:30 +0500

    updating client side

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,45 @@
+"use client"
+
 import Image from "next/image"
 import {
   ValidatorTable,
   BlocksTable,
   NodeOperatorsTable,
 } from "@/components/tables"
-import { getValidators, getBlocks, getNodeOperators } from "@/utils/apiCalls"
-import { startValidatorUpdateInterval, validatorUpdate } from "@/utils/database"
+import { getBlocks, getNodeOperators, getValidators } from "@/utils/apiCalls"
+// import { startValidatorUpdateInterval, validatorUpdate } from "@/utils/database"
+
 import { useEffect, useState } from "react"
 
-export default async function Home() {
-  const validatorResult = await getValidators()
-  await startValidatorUpdateInterval()
+export default function Home() {
+  const [validators, setValidators] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchedData = await getValidators()
+
+        console.log("fetchedData", fetchedData)
+
+        setValidators(fetchedData.data)
+      } catch (error) {
+        setError(error.message)
+      }
+    }
+
+    fetchData() // Initial fetch
+
+    // Set up interval for subsequent fetches
+    const intervalId = setInterval(fetchData, 5000)
+    // console.log("data", data)
+    // Cleanup function to clear the interval
+    return () => clearInterval(intervalId)
+  }, [])
+
+  // const validatorResult = await getValidators()
+  // await startValidatorUpdateInterval()
   // const blocksResult = await getBlocks()
   // const nodeResult = await getNodeOperators()
   // const updateResult = await validatorUpdate()
@@ -20,10 +49,8 @@ export default async function Home() {
 
   return (
     <>
-      {validatorResult ? (
-        <>
-          <ValidatorTable list={validatorResult.data} />
-        </>
+      {validators ? (
+        <ValidatorTable list={validators} />
       ) : (
         <>Fetching Validators ...</>
       )}
